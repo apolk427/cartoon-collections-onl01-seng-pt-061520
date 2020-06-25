bba01210c4e24c4a61a43a7ffcc9f93437a73773
Amyre Polk <myremyre101@icloud.com>
2020-06-25 17:04:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,30 +1,29 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-	planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-calls_screams = ["bo", "bam", "boom", "bazam", "powzer"]
+def roll_call_dwarves(array)
+  array.each_with_index {|name,index| puts "#{index + 1} #{name}"}
+end
 
-def roll_call_dwarves
-  array.each_with_index do |name, index|
-    puts "#{index + 1}. #{name}"
+def summon_captain_planet(planeteer_calls)
+   planeteer_calls.map {|element| element.capitalize + "!"}
+end
+
+def long_planeteer_calls(words)
+   if words.find{|word| word.length > 4}
+     true
+   else
+     false
    end
 end
 
-def summon_captain_planet
-  calls.map do |name|
-        name.capitalize << "!"
-      end
-end
-
-
-
-def long_planeteer_calls
-  planteer_calls.all? {|word| word.length < 4 }
-  planteer_calls.any? {|word| word.length > 4 }
-   
-end
-
-def find_the_cheese
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    cheese_array.find do |cheese|
-      cheese_types.include?(cheese)
-  end
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  if  strings.include? "cheddar"
+    return "cheddar"
+  elsif strings.include? "gouda"
+    return "gouda"
+  elsif strings.include? "camembert"
+    return "camembert"
+ else
+   nil 
 end
+end	end
